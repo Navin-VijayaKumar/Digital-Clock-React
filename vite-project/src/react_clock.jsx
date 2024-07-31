81b8a2c209050a4e6b208adc4ee75e0c0158e072
navin129 <navinv.22cse@kongu.edu>
2024-07-31 13:55:01 +0530

    secont commit

--- vite-project/src/react_clock.jsx
+++ vite-project/src/react_clock.jsx
@@ -3,8 +3,6 @@ import React,{useState,useEffect} from "react";
 function Clock_react()
 {
     const [time,setTime]=useState(new Date());
-
-
    useEffect(()=>{
     const intervalId=setInterval(()=>{
         setTime(new Date());
